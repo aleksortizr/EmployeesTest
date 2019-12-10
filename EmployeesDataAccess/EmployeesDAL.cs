a0ce667d98a4605e58358b8190f19733f8a03881
Alexander <aleksortizr@gmail.com>
2019-12-10 16:39:43 -0500

    Removed Fs files

--- EmployeesDataAccess/EmployeesDAL.cs
+++ EmployeesDataAccess/EmployeesDAL.cs
@@ -12,20 +12,9 @@ namespace Employees_DataAccess
 
         public async Task<List<EmployeesDTO>> GetAllEmployees()
         {
-            try
-            {
-                using var client = new HttpClient();
-                string result = await client.GetStringAsync("http://masglobaltestapi.azurewebsites.net/api/Employees");
-
-                var employees = Newtonsoft.Json.JsonConvert.DeserializeObject<List<EmployeesDTO>>(result);
-
-                return employees;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return new List<EmployeesDTO>();
-            }
+            using var client = new HttpClient();
+            string result = await client.GetStringAsync("http://masglobaltestapi.azurewebsites.net/api/Employees");
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<List<EmployeesDTO>>(result); 
         }
     }
 }
